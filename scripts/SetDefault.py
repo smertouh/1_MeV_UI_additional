355ab0a03fe08d94d89e9d68a2c48787ca7ea3ed
Andrey Sanin <sanin_andrey@mail.ru>
2020-02-09 11:27:21 +0700

    commit

--- scripts/SetDefault.py
+++ scripts/SetDefault.py
@@ -1,15 +1,40 @@
 #print('SetDefault')
-self.checkBox_8.setChecked(True)
-self.checkBox_9.setChecked(False)
-self.checkBox_10.setChecked(True)
-self.checkBox_11.setChecked(True)
-self.checkBox_12.setChecked(True)
-self.checkBox_13.setChecked(True)
-self.checkBox_14.setChecked(True)
-self.checkBox_15.setChecked(False)
-self.checkBox_16.setChecked(True)
-self.checkBox_17.setChecked(True)
-self.checkBox_18.setChecked(True)
-self.checkBox_19.setChecked(True)
-self.spinBox_11.setValue(6500)
+self.checkBox_8.setChecked(True)  #chan0
+#self.spinBox_10.setValue(0)  #start
+#self.spinBox_11.setValue(1000)  #stop
+self.checkBox_9.setChecked(False)  #chan1
+#self.spinBox_12.setValue(0)  #start
+#self.spinBox_13.setValue(1000)  #stop
+self.checkBox_10.setChecked(True)  #chan2
+#self.spinBox_14.setValue(0)  #start
+#self.spinBox_15.setValue(1000)  #stop
+self.checkBox_11.setChecked(True)  #chan3
+#self.spinBox_16.setValue(0)  #start
+#self.spinBox_17.setValue(1000)  #stop
+self.checkBox_12.setChecked(True)  #chan4
+#self.spinBox_18.setValue(0)  #start
+#self.spinBox_19.setValue(1000)  #stop
+self.checkBox_13.setChecked(True)  #chan5
+#self.spinBox_20.setValue(0)  #start
+#self.spinBox_21.setValue(1000)  #stop
+self.checkBox_14.setChecked(True)  #chan6
+#self.spinBox_22.setValue(0)  #start
+#self.spinBox_23.setValue(1000)  #stop
+self.checkBox_15.setChecked(False)  #chan7
+#self.spinBox_24.setValue(0)  #start
+#self.spinBox_25.setValue(1000)  #stop
+self.checkBox_16.setChecked(True)  #chan8
+#self.spinBox_26.setValue(0)  #start
+#self.spinBox_27.setValue(1000)  #stop
+self.checkBox_17.setChecked(True)  #chan9
+#self.spinBox_28.setValue(0)  #start
+#self.spinBox_29.setValue(1000)  #stop
+self.checkBox_18.setChecked(True)  #chan10
+#self.spinBox_30.setValue(0)  #start
+#self.spinBox_31.setValue(1000)  #stop
+self.checkBox_19.setChecked(True)  #chan11
+#self.spinBox_32.setValue(0)  #start
+#self.spinBox_33.setValue(1000)  #stop
+#self.spinBox_34.setValue(0)  #ADC start
+#self.spinBox_35.setValue(1000)  #ADC stop
 
