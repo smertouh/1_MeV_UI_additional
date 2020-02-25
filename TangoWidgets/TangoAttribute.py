ef4589c05b5767baf6b7b1867d45b7c0a11f105a
salinsalins <sanin_andrey@mail.ru>
2020-02-25 11:59:28 +0700

    commit 2

--- TangoWidgets/TangoAttribute.py
+++ TangoWidgets/TangoAttribute.py
@@ -12,14 +12,15 @@ from PyQt5.QtWidgets import QWidget
 import tango
 
 from .Utils import *
+from .TangoWidget import TangoWidget
 
 class TangoAttribute:
     def __init__(self, name: str, level=logging.DEBUG):
         # defaults
-        self.name = str(name)
-        self.dn = ''
-        self.an = ''
-        self.dp = None
+        self.full_name = str(name)
+        self.device_name = ''
+        self.attribute_name = ''
+        self.device_proxy = None
         self.attr = None
         self.config = None
         self.format = None
@@ -29,11 +30,11 @@ class TangoAttribute:
         self.logger = config_logger(level=level)
         try:
             n = name.rfind('/')
-            self.dn = name[:n]
-            self.an = name[n+1:]
+            self.device_name = name[:n]
+            self.attribute_name = name[n + 1:]
             self.connect()
         except:
-            self.logger.warning('Can not create attribute %s', self.name)
+            self.logger.warning('Can not create attribute %s', self.full_name)
             self.logger.log(logging.DEBUG, 'Exception:', exc_info=True)
             self.connected = False
         self.time = time.time()
@@ -46,31 +47,29 @@ class TangoAttribute:
             self.connected = False
             self.ex_count = 0
             self.time = time.time()
-            self.logger.debug('Attribute %s disconnected', self.name)
+            self.logger.debug('Attribute %s disconnected', self.full_name)
 
     def connect(self):
         try:
-            if self.dn in TangoWidget.DEVICES and TangoWidget.DEVICES[self.dn] is not None:
-                self.dp = TangoWidget.DEVICES[self.dn]
+            if self.device_name in TangoWidget.DEVICES and TangoWidget.DEVICES[self.device_name] is not None:
+                self.device_proxy = TangoWidget.DEVICES[self.device_name]
             else:
-                self.dp = tango.DeviceProxy(self.dn)
-                TangoWidget.DEVICES[self.dn] = self.dp
-            if not self.dp.is_attribute_polled(self.an):
-                self.logger.info('Recommended to switch polling on for %s', self.name)
-            self.dp.ping()
-            self.attr = self.dp.read_attribute(self.an)
+                self.device_proxy = tango.DeviceProxy(self.device_name)
+                TangoWidget.DEVICES[self.device_name] = self.device_proxy
+            self.device_proxy.ping()
+            self.attr = self.device_proxy.read_attribute(self.attribute_name)
             self.update_config()
             self.connected = True
             self.time = time.time()
-            self.logger.info('Connected to Attribute %s', self.name)
+            self.logger.info('Connected to Attribute %s', self.full_name)
         except:
-            self.logger.warning('Can not create attribute %s', self.name)
+            self.logger.warning('Can not create attribute %s', self.full_name)
             self.logger.log(logging.DEBUG, 'Exception:', exc_info=True)
             self.connected = False
             self.time = time.time()
 
     def update_config(self):
-        self.config = self.dp.get_attribute_config_ex(self.an)[0]
+        self.config = self.device_proxy.get_attribute_config_ex(self.attribute_name)[0]
         self.format = self.config.format
         try:
             self.coeff = float(self.config.display_unit)
@@ -79,21 +78,20 @@ class TangoAttribute:
 
     def read(self, force=False):
         try:
-            if not force and self.dp.is_attribute_polled(self.an):
-                attrib = self.dp.attribute_history(self.an, 1)[0]
+            if not force and self.device_proxy.is_attribute_polled(self.attribute_name):
+                attrib = self.device_proxy.attribute_history(self.attribute_name, 1)[0]
                 if attrib.time.tv_sec > self.attr.time.tv_sec or \
                         (attrib.time.tv_sec == self.attr.time.tv_sec and attrib.time.tv_usec > self.attr.time.tv_usec):
                     self.attr = attrib
             else:
-                self.attr = self.dp.read_attribute(self.an)
-        except Exception as ex:
+                self.attr = self.device_proxy.read_attribute(self.attribute_name)
+        except:
             self.attr = None
             self.disconnect()
-            raise ex
+            raise
         self.ex_count = 0
-        return self.attr
 
     def write(self, value):
         #if self.readonly:
         #    return
-        self.dp.write_attribute(self.an, value/self.coeff)
+        self.device_proxy.write_attribute(self.attribute_name, value / self.coeff)
