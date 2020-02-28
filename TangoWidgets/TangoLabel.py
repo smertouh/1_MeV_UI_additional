8de8bf6f95f6bef86b55ee33e2c2a90150b9de7c
salinsalins <sanin_andrey@mail.ru>
2020-02-28 13:07:51 +0700

    commit 4

--- TangoWidgets/TangoLabel.py
+++ TangoWidgets/TangoLabel.py
@@ -11,26 +11,31 @@ import tango
 
 class TangoLabel(TangoWidget):
     def __init__(self, name, widget: QLabel, prop=None, refresh=False):
+        super().__init__(name, widget, readonly=True)
         self.property = prop
+        if self.property is None:
+            self.database = None
+        else:
+            self.database = tango.Database()
         self.property_value = None
         self.refresh = refresh
-        super().__init__(name, widget, readonly=True)
+
+    def read_property(self):
+        if self.database is None:
+            self.database = tango.Database()
+        self.property_value = self.database.get_device_attribute_property(self.attribute.device_name, self.attribute.attribute_name)[self.attribute.attribute_name][self.property][0]
 
     def read(self, force=False):
         if self.property is None:
             super().read()
             return
-        if self.attr is None:
-            super().read()
         if self.refresh or self.property_value is None:
-            db = tango.Database()
-            self.property_value = db.get_device_attribute_property(self.dn, self.an)[self.an][self.property][0]
-            #self.property_value = self.dp.get_property(self.an, self.property)[self.property][0]
-        return self.attr
+            self.read_property()
+        return
 
     def set_widget_value(self):
         if self.property is None:
             super().set_widget_value()
         else:
-            self.widget.setText(self.property_value)
+            self.widget.setText(str(self.property_value))
 
