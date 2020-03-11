83b3b6fa5d24aa5f572cd4c0119078d03303efa7
Sanin <sanin@inp.nsk.su>
2020-03-11 12:03:32 +0700

    commit

--- RF_System_UI.bat
+++ RF_System_UI.bat
@@ -1,2 +1,3 @@
 call c:\ProgramData\Anaconda3\Scripts\activate.bat
 call python RF_System_UI.py
+pause
--- Timer_UI.py
+++ Timer_UI.py
@@ -136,7 +136,9 @@ class MainWindow(QMainWindow):
             TangoAbstractSpinBox('binp/nbi/adc0/Acq_start', self.spinBox_34),  # adc start
             TangoAbstractSpinBox('binp/nbi/adc0/Acq_stop', self.spinBox_35),   # adc stop
         )
-        self.timer_on_led = TangoLED('binp/nbi/timing/', self.pushButton_29),  # oven
+        # timer on
+        #self.timer_on_led = Timer_on_LED('binp/nbi/timing/channel_state0', self.pushButton_29),  # timer on led
+        #self.rdwdgts.append(self.timer_on_led)
         # additional decorations
         self.single_periodical_callback(self.comboBox.currentIndex())
         # Connect signals with slots
