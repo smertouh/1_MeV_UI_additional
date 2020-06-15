b3ebf94d6bb6f0f1587b208706142c3f7297096e
salinsalins <sanin_andrey@mail.ru>
2020-06-15 18:21:35 +0700

    commit

--- Test_asyncio.py
+++ Test_asyncio.py
@@ -1,43 +1,99 @@
+# modified fetch function with semaphore
+import random
+import time
+import types
 import asyncio
-from datetime import datetime
+from aiohttp import ClientSession
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        delay = response.headers.get("DELAY")
+        date = response.headers.get("DATE")
+        print("{}:{} with delay {}".format(date, response.url, delay))
+        return await response.read()
 
 
-def stamp(*args):
-    print(datetime.now().strftime('%X.%f'), *args)
+async def bound_fetch(sem, url, session):
+    # Getter function with semaphore.
+    async with sem:
+        await fetch(url, session)
 
 
-async def switch():
-    stamp('switch')
+async def run(r):
+    url = "http://localhost:8080/{}"
+    tasks = []
+    # create instance of Semaphore
+    sem = asyncio.Semaphore(1000)
 
+    # Create client session that will ensure we dont open new connection
+    # per each request.
+    async with ClientSession() as session:
+        for i in range(r):
+            # pass Semaphore and session to every GET request
+            task = asyncio.ensure_future(bound_fetch(sem, url.format(i), session))
+            tasks.append(task)
+
+        responses = asyncio.gather(*tasks)
+        await responses
+
+#number = 10000
+#loop = asyncio.get_event_loop()
+
+#uture = asyncio.ensure_future(run(number))
+#loop.run_until_complete(future)
 
 async def say_after(delay, what):
-    stamp('say_after(%.2f, %s) entry' % (delay, what))
     await asyncio.sleep(delay)
-    stamp('say_after(%.2f, %s) exit' % (delay, what))
+    print(what)
+
+
+@types.coroutine
+def __yield():
+    """Skip one event loop run cycle.
+
+    It uses a bare 'yield' expression
+    (which Task.__step knows how to handle)
+    instead of creating a Future object.
+    """
+    yield
+
+def a(x=1, y=1, z=3, *args, **kwargs):
+    print(x, y, z)
+    print(args)
+    print(kwargs)
 
 
 async def main():
-    stamp("main: started")
-
     task1 = asyncio.create_task(
         say_after(1, 'hello'))
+
     task2 = asyncio.create_task(
         say_after(2, 'world'))
-    stamp("main: tasks created")
+
+    print(f"started at {time.strftime('%X')}")
 
     # Wait until both tasks are completed (should take
     # around 2 seconds.)
+    #await task1
+    #await task2
+    #await asyncio.sleep(3)
+    #print(asyncio.all_tasks())
+    #while True:
+    #    print(task1.done(), task2.done())
+    #    if task1.done():
+    #        print(task1.result())
+    #    await asyncio.sleep(0.1)
+    loop = asyncio.get_event_loop()
+    #loop.run_until_complete()
+    while not task2.done():
+        await __yield()
 
-    await switch()
-    stamp("main: after await switch")
-    await asyncio.sleep(0)
-    stamp("main: after await asyncio.sleep(0)")
+    print(f"finished at {time.strftime('%X')}")
 
-    await asyncio.sleep(2.2)
-    #await say_after(2.2, 'velo')
-    #stamp("main: after first await")
-    #await say_after(1.1, 'moto')
-
-    stamp("main: finished")
+#loop = asyncio.events.new_event_loop()
+#loop.run_forever()
+#task1 = asyncio.create_task(say_after(1, 'hello'))
+#task2 = asyncio.create_task(say_after(2, 'hello'))
+#task3 = asyncio.create_task(say_after(3, 'hello'))
 
 asyncio.run(main())
